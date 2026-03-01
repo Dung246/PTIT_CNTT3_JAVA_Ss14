8f0173b59b555e2ef57ae3b6ac4750f8e9600df7
Dung246 <dungoo2605@gmail.com>
2026-03-01 14:19:02 +0700

    Update B1.java

--- src/homework/B1.java
+++ src/homework/B1.java
@@ -1,4 +1,24 @@
 package homework;
+import java.util.*;
 
 public class B1 {
+    public static void main(String[] args) {
+        String[] input = {
+                "Nguyễn Văn A – Yên Bái",
+                "Trần Thị B – Thái Bình",
+                "Nguyễn Văn A – Yên Bái",
+                "Lê Văn C – Hưng Yên"
+        };
+
+        Set<String> list = new LinkedHashSet<>();
+
+        for (String name : input) {
+            list.add(name);
+        }
+
+        System.out.println("Danh sách gọi khám:");
+        for (String ten : list) {
+            System.out.print(ten + ", ");
+        }
+    }
 }
