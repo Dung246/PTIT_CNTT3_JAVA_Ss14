3d0da03a9431254afb77122d8a7fb2c0e8e464be
Dung246 <dungoo2605@gmail.com>
2026-03-01 14:20:02 +0700

    Update B4.java

--- src/homework/B4.java
+++ src/homework/B4.java
@@ -1,4 +1,21 @@
 package homework;
-
+import java.util.*;
 public class B4 {
+    public static void main(String[] args) {
+        List<String> diseaseList = Arrays.asList(
+                "Cúm A",
+                "Sốt xuất huyết",
+                "Cúm A",
+                "Covid-19",
+                "Cúm A",
+                "Sốt xuất huyết"
+        );
+        Map<String, Integer> diseaseCount = new TreeMap<>();
+        for (String disease : diseaseList) {
+            diseaseCount.put(disease, diseaseCount.getOrDefault(disease, 0) + 1);
+        }
+        for (Map.Entry<String, Integer> entry : diseaseCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " ca");
+        }
+    }
 }
