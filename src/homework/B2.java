2dc3b597d5ed7bacf9d4ccafa0655be92038a65e
Dung246 <dungoo2605@gmail.com>
2026-03-01 14:19:17 +0700

    Update B2.java

--- src/homework/B2.java
+++ src/homework/B2.java
@@ -1,4 +1,24 @@
 package homework;
 
+import java.util.*;
+
 public class B2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> drugCatalog = new HashMap<>();
+        drugCatalog.put("T01", "Paracetamol");
+        drugCatalog.put("T02", "Ibuprofen");
+        drugCatalog.put("T03", "Axxit amin");
+        drugCatalog.put("T04", "Vitamin C");
+        drugCatalog.put("T05", "Aspirin");
+
+        System.out.print("Nhập tên thuốc: ");
+        String drugCode = scanner.nextLine().trim().toUpperCase();
+        if (drugCatalog.containsKey(drugCode)) {
+            System.out.println("Tên thuốc: " + drugCatalog.get(drugCode));
+        } else {
+            System.out.println("Không tồn tại thuốc này");
+        }
+        scanner.close();
+    }
 }
