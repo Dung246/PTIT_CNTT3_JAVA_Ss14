79f84043d42567135a4ed0835de2ff1ef424251a
Dung246 <dungoo2605@gmail.com>
2026-03-01 14:19:43 +0700

    Add main method for allergy check in B3.java
    Implement main method to check for drug allergies and safe ingredients.

--- src/homework/B3.java
+++ src/homework/B3.java
@@ -1,4 +1,21 @@
 package homework;
 
+import java.util.*;
+
 public class B3 {
+    public static void main(String[] args) {
+        Set<String> drugIngredients = new HashSet<>(
+                Arrays.asList("Aspirin", "Caffeine", "Paracetamol")
+        );
+        Set<String> allergyList = new HashSet<>(
+                Arrays.asList("Penicillin", "Aspirin")
+        );
+        Set<String> allergicSubstances = new HashSet<>(drugIngredients);
+        allergicSubstances.retainAll(allergyList);
+        Set<String> safeIngredients = new HashSet<>(drugIngredients);
+        safeIngredients.removeAll(allergyList);
+
+        System.out.println("THuốc: " + allergicSubstances);
+        System.out.println("Dị ứng: " + safeIngredients);
+    }
 }
